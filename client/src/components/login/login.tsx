f1a965d4a7e3efa11a43667bdb043de50607ddb5
ayush--1 <ayush5april@gmail.com>
2023-01-28 00:15:36 +0530

    notes,login,signup ui added

---
+++ client/src/components/login/login.tsx
@@ -0,0 +1,35 @@
+import { Box, Button, Heading, Img, Input } from '@chakra-ui/react';
+import React from 'react';
+import './login.css';
+
+function Login() {
+  return (
+    <div>
+      <Box display='flex'>
+        <Img></Img>
+        <Box className='login' mt={20} pl={10} pr={10} pb={10} mx='auto' boxShadow='2xl'>
+          <Heading as='h2' size='xl' mb={5} mt={5}>
+            Login
+          </Heading>
+          <Box mt={5} gap='5'>
+            <Heading as='h2' size='md' mb={5} mt={5}>
+              Email:
+            </Heading>
+            <Input borderRadius='10' type='email' w='100%' placeholder='Email' p={8} pl={10} pr={10} width='xl' />
+          </Box>
+          <Box mt={5} gap='5'>
+            <Heading as='h2' size='md' mb={5} mt={5}>
+              password:
+            </Heading>
+            <Input borderRadius='10' type='password' w='100%' placeholder='password' p={8} pl={10} pr={10} width='xl' />
+          </Box>
+          <Button colorScheme='blue' bg='#ffc600' mt={10} width='xl' height={14} color='#09223d'>
+            Log in
+          </Button>
+        </Box>
+      </Box>
+    </div>
+  );
+}
+
+export default Login;
--- client/src/components/navbar/navbar.tsx
+++ client/src/components/navbar/navbar.tsx
@@ -2,7 +2,7 @@ import { Box, Button, ButtonGroup, Flex, Spacer } from '@chakra-ui/react';
 
 function Navbar(): JSX.Element {
   return (
-    <Box bg=' #09223d' mt={-2} pt={2} pb={4} color='white'>
+    <Box bg=' #09223d' mt={-2} pt={2} pb={4} color='white' boxShadow='2xl'>
       <Flex minWidth='max-content' mt={2} ml={2} mr={2}>
         <Flex gap='10rem'>
           <h3>nav</h3>
@@ -11,8 +11,12 @@ function Navbar(): JSX.Element {
         </Flex>
         <Spacer />
         <ButtonGroup gap='2'>
-          <Button colorScheme='teal'>Sign Up</Button>
-          <Button colorScheme='teal'>Log in</Button>
+          <Button colorScheme='blue' bg='#ffc600' color='#09223d'>
+            Sign Up
+          </Button>
+          <Button colorScheme='blue' bg='#ffc600' color='#09223d'>
+            Log in
+          </Button>
         </ButtonGroup>
       </Flex>
     </Box>
