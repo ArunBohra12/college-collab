491bff64716ad9e4304db16ef023e8b0a3216ff4
Arun Bohra <arunbohra12@gmail.com>
2023-01-29 19:23:47 +0530

    Add documentation for file upload utility functions

--- server/utils/file.js
+++ server/utils/file.js
@@ -1,5 +1,10 @@
 import fs from 'node:fs';
 
+/**
+ * It checks if a path exists and returns true if it does, false if it doesn't
+ * @param pathToCheck - The path (absolute path not relative) to check if it exists.
+ * @returns A boolean value.
+ */
 const checkPathExists = async (pathToCheck) => {
   try {
     await fs.promises.access(pathToCheck);
--- server/utils/multer.js
+++ server/utils/multer.js
@@ -3,6 +3,15 @@ import { v4 as uuidv4 } from 'uuid';
 
 import checkPathExists from './file.js';
 
+/**
+ * It creates a multer disk storage object that checks if the folder you're trying to upload to exists
+ * and returns a diskStorage object for multer
+ * @param filePath - The path where you want to upload the file.
+ * @param fileSuffix - Optional parameter to add a suffix to the file name.
+ * For example, if you're uploading a profile picture, you can pass the user's id as the suffix.
+ *
+ * @returns A function that takes in two arguments, filePath and fileSuffix.
+ */
 export const createMulterDiskStorage = (filePath, fileSuffix) => {
   const diskStorage = multer.diskStorage({
     destination: async (req, file, cb) => {
@@ -20,13 +29,18 @@ export const createMulterDiskStorage = (filePath, fileSuffix) => {
       const extension = file.mimetype.split('/')[1];
       const uniqueId = uuidv4();
 
-      cb(null, `${uniqueId}-${fileSuffix || ''}-${Date.now()}.${extension}`);
+      cb(null, `${uniqueId}-${Date.now()}-${fileSuffix || ''}.${extension}`);
     },
   });
 
   return diskStorage;
 };
 
+/**
+ * This function creates a file filter callback function to be used with multer
+ * @param filterOptions - An object containing filter options for multer
+ * @returns A function that takes in req, file, and cb as params
+ */
 export const createFileFilter = (filterOptions) => {
   const { suportedMimeTypes } = filterOptions;
 
