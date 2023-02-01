e6a94468a6293167b68d935115a6b0fe2f9b5db5
Arun Bohra <arunbohra12@gmail.com>
2023-02-01 23:56:37 +0530

    Remove chakra ui and its components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,7 @@
 import './app.css';
-import { ChakraProvider } from '@chakra-ui/react';
-
-// import theme from './theme';
-import NavbarLoggedin from './components/navbar/navbarloggedin';
-
-import theme from './chakraUi/theme';
-
-import Notes from './components/notes/notes';
 
 function App(): JSX.Element {
-  return (
-    <ChakraProvider theme={theme}>
-      <div className='app'>
-        <NavbarLoggedin />
-        {/* <NavbarLoggedOut /> */}
-        {/* <Bounties /> */}
-        <Notes />
-        {/* <Login /> */}
-        {/* <SignUp /> */}
-      </div>
-    </ChakraProvider>
-  );
+  return <div className='app'></div>;
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
-import colorTheme from './chakraUi/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <ChakraProvider theme={colorTheme}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </ChakraProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter>
 );
